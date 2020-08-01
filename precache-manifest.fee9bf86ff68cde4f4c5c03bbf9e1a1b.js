44894d743994eaf17cb5693192f46086891fe3fc
AlexCherny962 <53741736+AlexCherny962@users.noreply.github.com>
2020-08-01 06:55:23 +0300

    Updates

---
+++ precache-manifest.fee9bf86ff68cde4f4c5c03bbf9e1a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03218cb12359e18fab884aece0ba10ff",
+    "url": "/cra13-comments-add-dimension/index.html"
+  },
+  {
+    "revision": "42cd2796fb020ff52e2a",
+    "url": "/cra13-comments-add-dimension/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "003d634e5011ef7b22bc",
+    "url": "/cra13-comments-add-dimension/static/js/2.198e134a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra13-comments-add-dimension/static/js/2.198e134a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42cd2796fb020ff52e2a",
+    "url": "/cra13-comments-add-dimension/static/js/main.c0671576.chunk.js"
+  },
+  {
+    "revision": "6e31e7bdb9c2a359f813",
+    "url": "/cra13-comments-add-dimension/static/js/runtime-main.bcb7a938.js"
+  }
+]);
